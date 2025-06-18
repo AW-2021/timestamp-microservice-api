646a19dfa90ac5d40a252513025f82087e7a3c79
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-06-18 15:01:44 +0500

    Created a timestamp microservice API backend

--- index.js
+++ index.js
@@ -24,6 +24,42 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res) => {
+  const dateObj = new Date(req.params.date);
+
+  if (dateObj == "Invalid Date") {
+    const dateNumber = Number(req.params.date);
+    const dateObj2 = new Date(dateNumber);
+
+    if (dateObj2 !== "Invalid Date") {
+      const utcString2 = dateObj2.toUTCString();
+
+      res.json({
+        unix: dateNumber,
+        utc: utcString2,
+      });
+    } else {
+      res.json({ error: "Invalid Date" });
+    }
+  } else {
+    const utcString = dateObj.toUTCString();
+
+    res.json({
+      unix: Number(dateObj),
+      utc: utcString,
+    });
+  }
+});
+
+app.get("/api", (req, res) => {
+  const dateToday = Date.now(); // Unix timestamp in milliseconds
+  const dateTodayUTC = new Date(dateToday).toUTCString();
+
+  res.json({
+    unix: dateToday,
+    utc: dateTodayUTC,
+  });
+});
 
 
 // Listen on port set in environment variable or default to 3000
